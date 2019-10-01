3f3a63540f88e5b431d71500b0f69aecf99f462c
Mel Gomez <meldarrelgomez@gmail.com>
2019-10-01 18:48:23 +0800

    Implement CRUD API for company resource

--- server/controllers/companies.js
+++ server/controllers/companies.js
@@ -25,5 +25,35 @@ module.exports = {
     await newCompany.save()
 
     res.send({ msg: 'Successfully added company', company: newCompany })
+  },
+
+  update: async (req, res) => {
+    const { error } = validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    const { name } = req.body
+
+    const company = await Company.findByIdAndUpdate(
+      req.params.id,
+      { name },
+      { new: true }
+    )
+
+    if (!company)
+      res.status(404).send({ msg: 'Company with the given ID was not found' })
+
+    res.send({
+      msg: 'Company updated successfully!',
+      company
+    })
+  },
+  delete: async (req, res) => {
+    const company = await Company.findByIdAndRemove(req.params.id)
+    if (!company)
+      return res
+        .status(404)
+        .send({ msg: 'Company with the given ID was not found' })
+
+    res.send({ company, msg: `${company.name} successfully deleted!` })
   }
 }
--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const company = require('../controllers/companies')
 
 router.get('/', company.index)
+router.get('/:id', company.show)
 router.post('/', company.create)
+router.put('/:id', company.update)
+router.delete('/:id', company.delete)
 
 module.exports = router
